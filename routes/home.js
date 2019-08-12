d10484e2bd47ee5166beb7ed1686154c7ba9b9a0
13580096341 <13580096341@adc.com>
2019-08-12 15:00:25 +0800

    first commit

---
+++ routes/home.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+const checkLogin = require('../middlewares/check').checkLogin
+const HomeModel = require('../models/home')
+
+// GET /signin 登录页
+router.get('/', function (req, res, next) {
+    const author = req.query.author
+  
+    HomeModel.getPosts(author)
+      .then(function (posts) {
+        res.render('home', {
+          posts: posts
+        })
+      })
+      .catch(next)
+  })
+module.exports = router
